9c2dd751b34136e448c0b6001b65b4dd1e20b909
NabilThange <thangenabil@gmail.com>
2025-04-24 22:48:46 +0530

    smooth-try2

--- components/ui/smooth-cursor.tsx
+++ components/ui/smooth-cursor.tsx
@@ -83,8 +83,8 @@ const DefaultCursorSVG: FC = () => {
 export function SmoothCursor({
   cursor = <DefaultCursorSVG />,
   springConfig = {
-    damping: 30,
-    stiffness: 500,
+    damping: 55,
+    stiffness: 400,
     mass: 1,
     restDelta: 0.001,
   },

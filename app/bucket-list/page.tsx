1a85fb1d436b3d73ceb7d5f16301a6fb9b244fc4
NabilThange <thangenabil@gmail.com>
2025-04-24 16:51:36 +0530

    style: update styling for bucket list section

--- app/bucket-list/page.tsx
+++ app/bucket-list/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function BucketListPage() {
   return (
-    <section className="min-h-screen w-full bg-blue-500">
+    <section className="h-96 w-full bg-blue-500 m-8 rounded-lg">
       {/* Content for the blue section can go here */}
     </section>
   );

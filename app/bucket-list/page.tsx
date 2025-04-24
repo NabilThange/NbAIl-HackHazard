d23d71d2857e4480fb441fd2df81ca3a2886f7b4
NabilThange <thangenabil@gmail.com>
2025-04-24 16:44:10 +0530

    refactor: replace bucket list page content with simple blue section

---
+++ app/bucket-list/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function BucketListPage() {
+  return (
+    <section className="min-h-screen w-full bg-blue-500">
+      {/* Content for the blue section can go here */}
+    </section>
+  );
+}
